ee3a77d9eb2fa5efdce93814fa169d03f106f9e0
NataliyaLaz <nataliya.laz@gmail.com>
2022-04-28 22:34:10 +0300

    first lesson half commit

--- Fitness/Controllers/MainViewController.swift
+++ Fitness/Controllers/MainViewController.swift
@@ -8,6 +8,41 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    private let userPhotoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = #colorLiteral(red: 0.7607843137, green: 0.7607843137, blue: 0.7607843137, alpha: 1)
+        imageView.layer.borderWidth = 5
+        imageView.layer.borderColor = UIColor.white.cgColor
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let calendarView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .specialGreen
+        view.layer.cornerRadius = 10
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let userNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "User Name"
+        label.textColor = .specialGray
+        label.font = .robotoMedium24()
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.5
+        //label.numberOfLines = 2
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override func viewDidLayoutSubviews() {
+        userPhotoImageView.layer.cornerRadius = userPhotoImageView.frame.width / 2
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +57,10 @@ class MainViewController: UIViewController {
         
         view.backgroundColor = .specialBackground
         
+        view.addSubview(calendarView)
+        view.addSubview(userPhotoImageView)
+        view.addSubview(userNameLabel)
+        
         
     }
 
@@ -31,5 +70,26 @@ extension MainViewController {
     
     private func setConstraints() {
         
+        NSLayoutConstraint.activate([
+            userPhotoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            userPhotoImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            userPhotoImageView.heightAnchor.constraint(equalToConstant: 100),
+            userPhotoImageView.widthAnchor.constraint(equalToConstant: 100)
+                                    
+        ])
+        
+        NSLayoutConstraint.activate([
+            calendarView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            calendarView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            calendarView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            calendarView.heightAnchor.constraint(equalToConstant: 70)
+                                    
+        ])
+        
+        NSLayoutConstraint.activate([
+            userNameLabel.leadingAnchor.constraint(equalTo: userPhotoImageView.trailingAnchor, constant: 5),
+            userNameLabel.bottomAnchor.constraint(equalTo: calendarView.topAnchor, constant: -10),
+            userNameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10)
+        ])
     }
 }
